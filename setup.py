401a07e3329373a49d63033f1ac9098f0099995a
viperadnan-git <viperadnan@gmail.com>
2024-09-01 14:41:55 +0530

    add description and python version

--- setup.py
+++ setup.py
@@ -14,6 +14,12 @@ def get_version():
         return version_match.group(1)
     raise RuntimeError("Unable to find version string.")
 
+# Function to read the README.md file for the long description
+def read_long_description():
+    readme_path = os.path.join(os.path.dirname(__file__), 'README.md')
+    with open(readme_path, 'r', encoding='utf-8') as f:
+        return f.read()
+
 setup(
     name="mailtm-python",
     version=get_version(),  # Dynamically get the version
@@ -22,6 +28,8 @@ setup(
         "requests",
     ],
     description="A Python SDK for interacting with the Mail.tm API",
+    long_description=read_long_description(),  # Add long description from README.md
+    long_description_content_type="text/markdown",  # Specify the format of the long description
     author="Adnan Ahmad",
     author_email="viperadnan@gmail.com",
     url="https://github.com/viperadnan-git/mailtm-python-sdk",
